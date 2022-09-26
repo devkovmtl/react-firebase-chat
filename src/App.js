ef25e30088568cf21adeee4f9ef558e1579cd848
Mikael <dev.kov.mtl@gmail.com>
2022-09-26 16:53:49 -0400

    Home

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import "./style.scss";
+
 function App() {
-  return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
-  );
+  return <Home />;
 }
 
 export default App;
