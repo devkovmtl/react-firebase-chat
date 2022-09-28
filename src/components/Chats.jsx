11304a38a94c2df5a95861ff0369b0e6b89b4010
Mikael <dev.kov.mtl@gmail.com>
2022-09-28 09:52:58 -0400

    Chats component layout and style

---
+++ src/components/Chats.jsx
@@ -0,0 +1,18 @@
+const Chats = () => {
+  return (
+    <div className="chats">
+      <div className="userChat">
+        <img
+          src="https://images.pexels.com/photos/1181742/pexels-photo-1181742.jpeg?auto=compress&cs=tinysrgb&w=600"
+          alt=""
+        />
+        <div className="userChatInfo">
+          <span>Jane</span>
+          <p>Hello</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Chats;
