5f2be0b14cbccbf0259247696650c99b8585374d
Mikael <dev.kov.mtl@gmail.com>
2022-09-28 14:55:25 -0400

    Added react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import { Routes, Route } from "react-router-dom";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import "./style.scss";
 
 function App() {
-  return <Home />;
+  return (
+    <Routes>
+      <Route path="/">
+        <Route index element={<Home />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
