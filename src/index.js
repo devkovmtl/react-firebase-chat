286be362f8252b4ab6ce0ce86854976749eb2dfa
Mikael <dev.kov.mtl@gmail.com>
2022-09-29 15:35:09 -0400

    Chat context provider created and added to app

--- src/index.js
+++ src/index.js
@@ -2,15 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import { AuthContextProvider } from "./context/AuthContext";
+import { ChatContextProvider } from "./context/ChatContext";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <AuthContextProvider>
-      <Router>
-        <App />
-      </Router>
+      <ChatContextProvider>
+        <Router>
+          <App />
+        </Router>
+      </ChatContextProvider>
     </AuthContextProvider>
   </React.StrictMode>
 );
